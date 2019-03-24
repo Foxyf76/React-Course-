a5224846a63bfa74adab59d72e7932dda69ef188
Foxyf76 <lukefox5@gmail.com>
2019-03-24 15:40:19 +0000

    Finished Vid 15

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
-import sampleFishes from '../sample-fishes';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
-	state = {
-		fishes: {},
-		order: {}
-	};
-	addFish = (fish) => {
-		const fishes = { ...this.state.fishes }; // '...' object spread, takes copy of the object
-		fishes[`fish${Date.now()}`] = fish;
-		this.setState({
-			fishes: fishes // let the copy of fishes equal the state.fishes
-		});
-	};
-	loadSampleFishes = () => {
-		this.setState({ fishes: sampleFishes });
-	};
-	render() {
-		return (
-			<div className="catch-of-the-day">
-				<div className="menu">
-					<Header tagline="Fresh Seafood Market" />
-				</div>
-				<Order />
-				<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
-			</div>
-		);
-	}
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    const fishes = { ...this.state.fishes }; // '...' object spread, takes copy of the object
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      fishes: fishes // let the copy of fishes equal the state.fishes
+    });
+  };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
+        </div>
+        <Order />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
